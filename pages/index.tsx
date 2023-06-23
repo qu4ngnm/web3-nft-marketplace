1eaab0f150feb06fda6b4773fd0fe03ae014d9e8
miiajj <nguyenngochuy242@gmail.com>
2023-06-23 20:41:36 +0700

    docs: remove duplicate words

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
           <div className="text-center">
             <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Các tác phẩm NFT tuyệt vời</h2>
             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-              Tạo một NFT để để có quyền sở hữu vĩnh viễn không giới hạn!
+              Tạo một NFT để có quyền sở hữu vĩnh viễn không giới hạn!
             </p>
           </div>
           { network.isConnectedToNetwork ?
