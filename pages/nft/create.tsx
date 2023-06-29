1779010d2e59627a3263699a2b6e16ea67fe6e30
miiajj <nguyenngochuy242@gmail.com>
2023-06-29 13:11:41 +0700

    1

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -272,7 +272,7 @@ const NftCreate: NextPage = () => {
                       type="button"
                       className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                      Đăng bán
+                      Tạo
                     </button>
                   </div>
                 </div>
